e56b958936cb716719b48f7703d66e6dcfb8246a
sotirelisc <gtakriz@hotmail.com>
2017-06-18 16:32:13 +0300

    Create basic Houses model.

---
+++ api/models/houseModel.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var HouseSchema = new Schema({
+  short_description: {
+    type: String
+  },
+  long_description: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  featured_image: {
+    type: String
+  },
+  long: {
+    type: String
+  },
+  lat: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'active']
+    }],
+    default: ['pending']
+  },
+  listing_type: {
+    type: [{
+      type: String,
+      enum: ['rent', 'buy']
+    }]
+  }
+})
+
+module.exports = mongoose.model('Houses', HouseSchema)
