4fead2fa52d33a187879371f2cb5b9af2ad2aa14
sotirelisc <gtakriz@hotmail.com>
2017-06-22 23:51:46 +0300

    Add API versioning for House endpoints.

--- api/routes/houseRoutes.js
+++ api/routes/houseRoutes.js
@@ -4,8 +4,9 @@ var passport = require('passport')
 
 module.exports = function(app) {
   var house = require('../controllers/houseController')
+  var versioning = require('../config/versioning')
 
-  app.route('/houses')
+  app.route(versioning.url + '/houses')
     .get(house.list_all_houses)
     .post(passport.authenticate('jwt', {
         session: false
@@ -13,7 +14,7 @@ module.exports = function(app) {
       function(req, res) {
         var token = getToken(req.headers)
         if (token) {
-          console.log("Creates a house: " + req.user)
+          // User from token is at req.user
           house.create_a_house(req, res)
         } else {
           return res.status(403).send({
@@ -23,12 +24,13 @@ module.exports = function(app) {
         }
       })
 
-  app.route('/houses/:houseId')
+  app.route(versioning.url + '/houses/:houseId')
     .get(house.read_a_house)
     .put(house.update_a_house)
     .delete(house.delete_a_house)
 }
 
+// JWT approach of getting token from request headers
 const getToken = (headers) => {
   if (headers && headers.authorization) {
     var parted = headers.authorization.split(' ')
