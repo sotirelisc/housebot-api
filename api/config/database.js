8c937cfacafa2aa1c239d27a15caf7599c264336
sotirelisc <gtakriz@hotmail.com>
2017-06-25 15:23:13 +0300

    Refactor getting process env vars.

--- api/config/database.js
+++ api/config/database.js
@@ -1,3 +1,9 @@
+// If locally, import vars from env.js
+// env.js is git ignored
+if (!process.env.PASSPORT_KEY || !process.env.MONGO_DB) {
+  var env = require('./env.js')
+}
+
 module.exports = {
   'secret': process.env.PASSPORT_KEY,
   'database': process.env.MONGO_DB
---
+++ api/config/env.js
@@ -0,0 +1,2 @@
+process.env['PASSPORT_KEY'] = "thewalkingdead"
+process.env['MONGO_DB'] = "mongodb://housebot:kanieloutis@ds131432.mlab.com:31432/housebot"
